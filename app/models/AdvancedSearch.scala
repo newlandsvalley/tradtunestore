064d2254a82191699972a901e2668cd61ba23596
John Watson <john.watson@gmx.co.uk>
2016-07-24 10:45:48 +0100

    Fixed bug in advanced search
    Fixed problem in URL-encoding advanced search terms.  The code used to URL encode the entire predicate, but now splits it up into an array of 'anded' terms, each representing an equality and then only encodes the rhs of the equality before reassembling.

--- app/models/AdvancedSearch.scala
+++ app/models/AdvancedSearch.scala
@@ -5,7 +5,32 @@ import java.net.URLEncoder
 
 case class AdvancedSearch(genre: String, criteria: String) {
 
-  def predicate() : Option[String] = Some(URLEncoder.encode(criteria, "UTF-8"))
+  def predicate() : Option[String] = urlEncode(criteria)
+
+  // URL encode a search predicate
+  def urlEncode(criteria: String) : Option[String] = {
+    if (0 == criteria.length()) {
+      None
+      }
+    else {
+      val andedTerms = criteria.split('&')
+      val encodedTerms = andedTerms.map(encodeTerm)
+      Some (encodedTerms.mkString("&"))
+    }
+  }
+
+  // URL encode an individual anded term in a search predicate
+  def encodeTerm(term: String) : String = {
+    val equalPos = term.indexOf('=')
+    if (-1 == equalPos) {
+      URLEncoder.encode(term, "UTF-8")
+      }
+    else {
+      val lhs = term.substring(0, equalPos)
+      val rhs = URLEncoder.encode(term.substring(equalPos +1, term.length()),  "UTF-8")
+      lhs + "=" + rhs
+    }
+  }
 
 }
 
